d8941c0e3f9e7cff4f39a974d0046b002ca47242
Nicholas Gallina <nicholasgallina1922@gmail.com>
2026-03-17 20:38:30 -0400

    feat: physical device selection

--- src/nre_device.rs
+++ src/nre_device.rs
@@ -4,14 +4,20 @@ use ash::Entry;
 pub struct NreDevice {
     entry: Entry,
     instance: ash::Instance,
+    physical_device: vk::PhysicalDevice,
 }
 
 impl NreDevice {
     pub fn new() -> Self {
         let entry = unsafe { Entry::load().unwrap() };
         let instance = Self::create_instance(&entry);
+        let physical_device = Self::choose_physical_device(&instance);
 
-        Self { entry, instance }
+        Self {
+            entry,
+            instance,
+            physical_device,
+        }
     }
 
     fn create_instance(entry: &Entry) -> ash::Instance {
@@ -26,5 +32,18 @@ impl NreDevice {
         };
 
         unsafe { entry.create_instance(&create_info, None).unwrap() }
+    }
+
+    fn choose_physical_device(instance: &ash::Instance) -> vk::PhysicalDevice {
+        let devices = unsafe { instance.enumerate_physical_devices().unwrap() };
+
+        devices
+            .into_iter()
+            .find(|device| Self::is_device_suitable(device))
+            .expect("no suitable GPU found")
+    }
+
+    fn is_device_suitable(_device: &vk::PhysicalDevice) -> bool {
+        true
     }
 }
