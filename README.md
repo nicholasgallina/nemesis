22d93550beddf856ad37eb19f1e50874f68ca7ee
Nicholas Gallina <nicholasgallina1922@gmail.com>
2026-04-16 19:31:19 -0400

    README + auto shader compilation

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 Nemesis is a Vulkan rendering engine written in Rust that simultaneously renders arbitrary
 3D meshes and instanced molecular structures from PDB data; built for scientific visualization of
 MOGAD-relevant proteins.
+
+## Dependencies
+
+- [Rust](https://rustup.rs) (stable)
+- [Vulkan SDK](https://vulkan.lunarg.com/) 1.3+
+- [glslc](https://github.com/google/shaderc)
+
+## Building
+
+```bash
+git clone https://github.com/nicholasgallina/nemesis.git
+cd nemesis
+cargo run
+```
---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::process::Command;
+
+fn main() {
+    let shaders = [
+        ("shaders/simple.vert", "shaders/simple.vert.spv"),
+        ("shaders/simple.frag", "shaders/simple.frag.spv"),
+        ("shaders/atom.vert", "shaders/atom.vert.spv"),
+        ("shaders/atom.frag", "shaders/atom.frag.spv"),
+    ];
+
+    for (src, out) in &shaders {
+        let status = Command::new("glslc")
+            .args([src, "-o", out])
+            .status()
+            .expect("ERROR: failed to run glslc");
+
+        if !status.success() {
+            panic!("ERROR: shader compilation failed: {}", src);
+        }
+
+        println!("cargo:rerun-if-changed={}", src);
+    }
+}
